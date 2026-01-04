0bdf282344b7d4c13816f372a49840cbdfc91799
Adarsh Agarwal <adarsh.agar211@gmail.com>
2026-01-04 23:39:55 +0100

    updated

--- js/chunk-XZVDIMEC.mjs
+++ js/chunk-XZVDIMEC.mjs
@@ -1,4 +1,4 @@
 function t(a,e){return{customHTMLBodyStart:`<script>
   document.body.style.overscrollBehavior = "none";
-<\/script>`,description:"PokoCoin, inspired by a beloved childhood cartoon series, brings nostalgia and excitement to crypto with Pokocat .",favicon:"https://framerusercontent.com/assets/1BkNY7ahYRaURY4FTdyKKiz87E.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/sQLYSImzCLqSyw3oBaOeL0Dz8g.png",title:"PokoCoin"}}export{t as a};
+<\/script>`,description:"PokoCoin, inspired by a beloved childhood cartoon series, brings nostalgia and excitement to crypto with Pokocat .",favicon:"../images/1BkNY7ahYRaURY4FTdyKKiz87E.png",robots:"max-image-preview:large",socialImage:"../images/sQLYSImzCLqSyw3oBaOeL0Dz8g.png",title:"PokoCoin"}}export{t as a};
 //# sourceMappingURL=chunk-XZVDIMEC.mjs.map
